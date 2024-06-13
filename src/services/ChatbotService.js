7ccc4e26f5ce182b72749b760f6906cb84b1878b
chiennguyen30 <nguyenchien30102001@gmail.com>
2024-06-13 16:44:00 +0700

    s

--- src/services/ChatbotService.js
+++ src/services/ChatbotService.js
@@ -26,10 +26,35 @@ let callSendAPI = async (sender_psid, response) => {
     }
   );
 };
+let getUserName = async (sender_psid) => {
+  let userName = "";
+  // Send the HTTP request to the Messenger Platform
+  await request(
+    {
+      uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "GET",
+    },
+    (err, res, body) => {
+      console.log(body);
+      if (!err) {
+        response = JSON.parse(res);
+        userName = response.first_name + " " + response.last_name;
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+
+  return userName;
+};
 let handleGetStarted = (sender_psid) => {
   return new Promise(async (resolve, reject) => {
     try {
-      let response = { text: "Xin chào bạn đến với Fullstack bạn cần mình giúp gì không?" };
+      let userName = await getUserName(sender_psid);
+      let response = {
+        text: `Xin chào bạn ${userName} đến với Fullstack bạn cần mình giúp gì không?`,
+      };
       await callSendAPI(sender_psid, response);
       resolve("done");
     } catch (error) {
