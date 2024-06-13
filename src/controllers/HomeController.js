30103b777038a95aa83ecdddabb6ddf6d1c1c605
chiennguyen30 <nguyenchien30102001@gmail.com>
2024-06-13 16:09:20 +0700

    s

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -122,6 +122,8 @@ function handlePostback(sender_psid, received_postback) {
     response = { text: "Thanks!" };
   } else if (payload === "no") {
     response = { text: "Oops, try sending another image." };
+  } else if (payload === "GET_STARTED") {
+    response = { text: "Xin chào bạn đến với Fullstack bạn cần mình giúp gì không?" };
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
