a844e4124c8b0fc1df4a7132e70cb4c02d3d61be
chiennguyen30 <nguyenchien30102001@gmail.com>
2024-06-13 16:17:19 +0700

    s

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -118,13 +118,21 @@ function handlePostback(sender_psid, received_postback) {
   let payload = received_postback.payload;
 
   // Set the response based on the postback payload
-  if (payload === "yes") {
-    response = { text: "Thanks!" };
-  } else if (payload === "no") {
-    response = { text: "Oops, try sending another image." };
-  } else if (payload === "GET_STARTED") {
-    response = { text: "Xin chào bạn đến với Fullstack bạn cần mình giúp gì không?" };
+  switch (payload) {
+    case "yes":
+      response = { text: "Thanks!" };
+      break;
+    case "no":
+      response = { text: "Oops, try sending another image." };
+      break;
+    case "GET_STARTED":
+      response = { text: "Xin chào bạn đến với Fullstack bạn cần mình giúp gì không?" };
+      break;
+    default:
+      response = { text: "Xin lỗi vì đã gây khó chịu cho bạn!" };
+      break;
   }
+
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
 }
