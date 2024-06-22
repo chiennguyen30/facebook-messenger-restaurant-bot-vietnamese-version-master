e374deace077ed43904b4ab14bfa74d1c077e8ad
chiennguyen30 <nguyenchien30102001@gmail.com>
2024-06-22 16:52:27 +0700

    commit lan 4

--- src/services/ChatbotService.js
+++ src/services/ChatbotService.js
@@ -30,33 +30,39 @@ const IMG_GIF_WELCOME = "https://bit.ly/3KRsau5";
 
 const IMG_DETAIL_ROOM = "https://bit.ly/eric-bot-18";
 let callSendAPI = async (sender_psid, response) => {
-  // Construct the message body
-  let request_body = {
-    recipient: {
-      id: sender_psid,
-    },
-    message: response,
-  };
+  return new Promise(async (resolve, reject) => {
+    try {
+      // Construct the message body
+      let request_body = {
+        recipient: {
+          id: sender_psid,
+        },
+        message: response,
+      };
 
-  await sendMarkReadMessage(sender_psid);
-  await sendTypingOn(sender_psid);
+      await sendMarkReadMessage(sender_psid);
+      await sendTypingOn(sender_psid);
 
-  // Send the HTTP request to the Messenger Platform
-  await request(
-    {
-      uri: "https://graph.facebook.com/v9.0/me/messages",
-      qs: { access_token: PAGE_ACCESS_TOKEN },
-      method: "POST",
-      json: request_body,
-    },
-    (err, res, body) => {
-      if (!err) {
-        console.log("message sent!");
-      } else {
-        console.error("Unable to send message:" + err);
-      }
+      // Send the HTTP request to the Messenger Platform
+      await request(
+        {
+          uri: "https://graph.facebook.com/v9.0/me/messages",
+          qs: { access_token: PAGE_ACCESS_TOKEN },
+          method: "POST",
+          json: request_body,
+        },
+        (err, res, body) => {
+          if (!err) {
+            resolve("message sent!");
+          } else {
+            console.error("Unable to send message:" + err);
+          }
+        }
+      );
+    } catch (error) {
+      reject(error);
     }
-  );
+  });
 };
 
 let sendTypingOn = async (sender_psid, response) => {
